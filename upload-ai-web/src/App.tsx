0314092f3e348aaf029f6ab8e51d24f7ff9a2528
renan04lima <renan04lima@outlook.com>
2023-09-12 13:56:45 -0300

    feat: add video upload and transcription prompt to form on right side

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,7 +1,8 @@
 import { Button } from "./components/ui/button"
-import { Github } from "lucide-react"
+import { Github, FileVideo, Upload } from "lucide-react"
 import { Separator } from "./components/ui/separator"
 import { Textarea } from "./components/ui/textarea"
+import { Label } from "./components/ui/label"
 
 export function App() {
   return (
@@ -39,7 +40,37 @@ export function App() {
           </p>
         </div>
 
-        <aside className="w-80"></aside>
+        <aside className="w-80 space-y-6">
+          <form action="" className="space-y-6">
+            <label 
+              htmlFor="video"
+              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+            >
+              <FileVideo className="h-4 w-4"/>
+              Carregar vídeo
+            </label>
+            <input type="file" id="video" accept="video/mp4"className="sr-only"/>
+
+            <Separator orientation="vertical" className="h-6"/>
+
+            <div className="space-y-6">
+              <Label htmlFor="transcription_prompt">
+                Prompt de transcrição
+              </Label>
+              <Textarea 
+                id="transcription_prompt" 
+                className="min-h-20 leading-relaxed"
+                placeholder="Inclua palavras-chaves mencionadas no vídeo separadas por vírgula (,)"
+              />
+            </div>
+
+            <Button type="submit" className="w-full">
+              Carregar vídeo
+              <Upload className="h-4 w-4 ml-2"/>
+            </Button>
+          </form>
+
+        </aside>
       </main>
     </div>
   )
