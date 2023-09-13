3722cd32bea03527998327aaad2703d839bec3ec
renan04lima <renan04lima@outlook.com>
2023-09-13 02:38:25 -0300

    feat: add transcription endpoint

--- upload-ai-api/src/index.ts
+++ upload-ai-api/src/index.ts
@@ -1,6 +1,7 @@
 import { fastify } from 'fastify'
 import { getAllPrompts } from './routes/get-all-prompts'
 import { uploadVideo } from './routes/upload-video'
+import { createTranscription } from './routes/create-transcription'
 
 const app = fastify()
 
@@ -10,6 +11,7 @@ app.get('/', () => {
 
 app.register(getAllPrompts)
 app.register(uploadVideo)
+app.register(createTranscription)
 
 app.listen({
     port: 3333
