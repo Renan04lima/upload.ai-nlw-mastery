f2368662d545ad0358edf3b4d8be7b8e545dfe26
renan04lima <renan04lima@outlook.com>
2023-09-13 12:55:28 -0300

    feat: add generate ai completion endpoint

--- upload-ai-api/src/index.ts
+++ upload-ai-api/src/index.ts
@@ -2,6 +2,7 @@ import { fastify } from 'fastify'
 import { getAllPrompts } from './routes/get-all-prompts'
 import { uploadVideo } from './routes/upload-video'
 import { createTranscription } from './routes/create-transcription'
+import { generateAICompletion } from './routes/generate-ai-completion'
 
 const app = fastify()
 
@@ -12,6 +13,7 @@ app.get('/', () => {
 app.register(getAllPrompts)
 app.register(uploadVideo)
 app.register(createTranscription)
+app.register(generateAICompletion)
 
 app.listen({
     port: 3333
