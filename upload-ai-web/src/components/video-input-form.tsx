b9995d85903893cc601f7b0aefdb64f42ec87c1c
renan04lima <renan04lima@outlook.com>
2023-09-14 17:55:21 -0300

    feat: convert video to audio

--- upload-ai-web/src/components/video-input-form.tsx
+++ upload-ai-web/src/components/video-input-form.tsx
@@ -4,6 +4,8 @@ import { Button } from "./ui/button";
 import { Textarea } from "./ui/textarea";
 import { Label } from "./ui/label";
 import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
+import { getFFmpeg } from "@/lib/ffmpeg";
+import { fetchFile } from "@ffmpeg/util";
 
 export function VideoInputForm() {
     const [videoFile, setVideoFile] = useState<File | null>(null)
@@ -21,12 +23,53 @@ export function VideoInputForm() {
 
     }
 
-    function handleUploadVideo(event: FormEvent<HTMLInputElement>) {
+    async function convertVideoToAudio(video: File) {
+        console.log('Convert Started...')
+
+        const ffmpeg = await getFFmpeg()
+
+        ffmpeg.writeFile('input.mp4', await fetchFile(video))
+
+        // ffmpeg.on('log', log => console.log(log))
+
+        ffmpeg.on('progress', progress => {
+            console.log('Convert progrees' + Math.round(progress.progress * 100))
+        })
+
+        await ffmpeg.exec([
+            '-i',
+            'input.mp4',
+            '-map',
+            '0:a',
+            '-b:a',
+            '20k',
+            '-acodec',
+            'libmp3lame',
+            'output.mp3'
+        ])
+
+        const data = await ffmpeg.readFile('output.mp3')
+
+        const audioFileBlob = new Blob([data], { type: 'audio/mpeg' })
+        const audioFile = new File([audioFileBlob], 'audio.mp3', {
+            type: 'audio/mpeg'
+        })
+
+        console.log('Convert Finished.')
+
+        return audioFile
+    }
+
+    async function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
         event.preventDefault()
 
         const prompt = promptInputRef.current?.value
 
-        if(!videoFile) return
+        if (!videoFile) return
+
+        const audioFile = await convertVideoToAudio(videoFile)
+
+        console.log(audioFile, prompt)
     }
 
     const previewURL = useMemo(() => {
@@ -36,7 +79,7 @@ export function VideoInputForm() {
     }, [videoFile])
 
     return (
-        <form className="space-y-6">
+        <form onSubmit={handleUploadVideo} className="space-y-6">
             <label
                 htmlFor="video"
                 className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
